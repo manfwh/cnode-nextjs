6f0c266a56e991725a955b2f661b945fc80e1790
manfwh <841533182@qq.com>
2020-08-16 21:42:16 +0800

    fix import nav

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { useRouter} from 'next/router'
 import Layout from '@/components/Layout'
-import Nav from '@/components/nav'
+import Nav from '@/components/Nav'
 import TopicsList from '@/components/TopicsList'
 
 export default function Home() {
